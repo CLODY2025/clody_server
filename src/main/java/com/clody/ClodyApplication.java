acea983d78368018d13450f6ab7cb72e0100df0b
1026hz <1026hzz@gmail.com>
2025-05-25 13:52:54 +0900

    📝docs(setting): 프로젝트 초기 세팅

---
+++ src/main/java/com/clody/ClodyApplication.java
@@ -0,0 +1,13 @@
+package com.clody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClodyApplication.class, args);
+	}
+
+}
