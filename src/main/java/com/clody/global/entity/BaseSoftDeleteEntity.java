7eaa5d560e509cba8a7f41b08d57ea4fc7e9c703
leeeemj <leemj2944@gmail.com>
2025-06-23 00:37:03 +0900

    :rocket: chore(entity): base entity 리팩토링

---
+++ src/main/java/com/clody/global/entity/BaseSoftDeleteEntity.java
@@ -0,0 +1,18 @@
+package com.clody.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseSoftDeleteEntity extends BaseTimeEntity {
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
