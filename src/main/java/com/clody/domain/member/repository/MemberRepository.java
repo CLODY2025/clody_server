32a5957a3bfbae5d0d17a3c9623c2a7ac945bdb5
leeeemj <leemj2944@gmail.com>
2025-08-15 15:49:59 +0900

    :sparkles: feat(member): member하드코딩 위해 추가

---
+++ src/main/java/com/clody/domain/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.clody.domain.member.repository;
+
+import com.clody.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findById(Long memberId);
+}
